dfbad67e9de6f4925518e9b88e3dd1bbf3306d3d
lauraguerrero01 <laura.j.guerrero.r@gmail.com0>
2025-05-14 16:25:06 -0500

    CONSULTA MULTITABLA 2 Laura

--- CONSULTAS MULTITABLA.sql
+++ CONSULTAS MULTITABLA.sql
@@ -35,9 +35,23 @@ SELECT p.nombre AS nombre_producto, p.precio FROM Producto p
 JOIN Fabricante f ON p.codigo_fabricante = f.codigo
 WHERE f.nombre = 'Crucial' AND p.precio > 200;
 
+-- 8. Productos de los fabricantes Asus, Hewlett-Packard y Seagate. Sin utilizar el operador IN.
+SELECT * FROM fabricante WHERE nombre = "Asus" OR nombre = "Hewlett-Packard" OR nombre = "Seagate";
 
+-- 9. Productos de los fabricantes Asus, Hewlett-Packard y Seagate. Utilizando el operador IN
+SELECT * FROM fabricante WHERE nombre IN ("Asus", "Hewlett-Packard", "Seagate");
 
+-- 10. Nombre y precio de todos los productos de los fabricantes cuyo nombre termine por la vocal e.
+SELECT f.nombre AS nombre_fabricante, p.nombre AS nombre_producto, p.precio FROM Producto p JOIN Fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre LIKE '%e';
 
+-- 11. Nombre y precio de todos los productos cuyo nombre de fabricante contenga el carácter w en su nombre.
+SELECT f.nombre AS nombre_fabricante, p.nombre AS nombre_producto, p.precio FROM Producto p JOIN Fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre LIKE '%w%';
+
+-- 12. Nombre de producto, precio y nombre de fabricante, de todos los productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente).
+SELECT p.nombre AS nombre_producto, precio * 0.92 AS precio_EUR, f.nombre AS nombre_fabricante FROM Producto p JOIN Fabricante f ON p.codigo_fabricante = f.codigo WHERE p.precio >= 180 ORDER BY p.precio DESC, p.nombre ASC;
+
+-- 13. Identificador y nombre de fabricante, solamente de aquellos fabricantes que tienen productos asociados en la base de datos.
+SELECT DISTINCT f.codigo AS codigo_fabricante, f.nombre AS nombre_fabricante FROM Fabricante f JOIN Producto p ON f.codigo = p.codigo_fabricante;
 
 
 -- COMPOSICIÓN EXTERNA --
